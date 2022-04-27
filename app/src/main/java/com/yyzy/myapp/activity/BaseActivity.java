6b62a161c5f0112e121a0e7d41b5146a21103687
Phang86 <1453061838@qq.com>
2022-04-27 09:05:01 +0800

    update

--- app/src/main/java/com/yyzy/myapp/activity/BaseActivity.java
+++ app/src/main/java/com/yyzy/myapp/activity/BaseActivity.java
@@ -16,7 +16,7 @@ import java.util.regex.Pattern;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
-    public static final String BASE_URLT = "http://192.168.1.101:8080/myapp";
+    public static final String BASE_URLT = "http://192.168.43.202:8080/myapp";
     public static final String BASE_URL = "http://192.168.88.202:8080/myapp";
     public Context context;
 
